8c9111370dd4e52daf39709b6c6833706b902518
Aditya Kapoor <adityakapoor074@gmail.com>
2022-05-16 12:31:05 +0530

    update

--- ma_gym/envs/traffic_junction/traffic_junction.py
+++ ma_gym/envs/traffic_junction/traffic_junction.py
@@ -54,9 +54,8 @@ class TrafficJunction(gym.Env):
     """
     metadata = {'render.modes': ['human', 'rgb_array']}
 
-    def __init__(self, grid_shape=(14, 14), step_cost=-0.01, n_max=4, collision_reward=-0.1, goal_reached_reward=1.0, arrive_prob=0.2,
+    def __init__(self, grid_shape=(14, 14), step_cost=-0.01, n_max=4, collision_reward=-0.1, goal_reached_reward=0.1, arrive_prob=0.2,
                  full_observable: bool = False, max_steps: int = 40):
-        # n_max = 6
         assert 1 <= n_max <= 10, "n_max should be range in [1,10]"
         assert 0 <= arrive_prob <= 1, "arrive probability should be in range [0,1]"
         assert len(grid_shape) == 2, 'only 2-d grids are acceptable'
